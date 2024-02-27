6c57e87efbb70d90ceb7e2433750d5d41c5bb1e1
Ar6yZuK <Ar6yZuK@gmail.com>
2024-02-27 18:59:30 +0300

    Добавьте файлы проекта.

---
+++ Base64Decoder/Decoder.cs
@@ -0,0 +1,29 @@
+﻿using System.Buffers;
+using System.Buffers.Text;
+using System.Text;
+
+Console.SetIn(new StreamReader(Console.OpenStandardInput(), Console.InputEncoding, false, 100_000));
+
+while (true)
+{
+    Console.Write("Write Base64 text to decode: ");
+    string? text = Console.ReadLine();
+	if (text is null)
+		return;
+
+	string result = Decode(text);
+
+	Console.WriteLine(result);
+}
+
+static string Decode(string textToDecode)
+{
+	int destinationLength = Base64.GetMaxDecodedFromUtf8Length(textToDecode.Length);
+	Span<byte> result = stackalloc byte[destinationLength];
+
+	OperationStatus operationResult = Base64.DecodeFromUtf8(Encoding.UTF8.GetBytes(textToDecode), result, out int byteConsumed, out int bytesWritten);
+	if (operationResult is not OperationStatus.Done)
+		throw new Exception(operationResult.ToString());
+
+	return Encoding.UTF8.GetString(result.Slice(0, bytesWritten));
+}
---
+++ Base64Encoder/Encoder.cs
@@ -0,0 +1,32 @@
+﻿using System.Buffers;
+using System.Buffers.Text;
+using System.Text;
+
+// from private Property in Base64Encoder = 1610612733
+const int MaximumEncodeLength = int.MaxValue / 4 * 3;
+
+Console.SetIn(new StreamReader(Console.OpenStandardInput(), Console.InputEncoding, false, MaximumEncodeLength));
+
+while (true)
+{
+	Console.Write("Write text to encode to Base64: ");
+	string? text = Console.ReadLine();
+	if (text is null)
+		return;
+
+	string result = Encode(text);
+
+	Console.WriteLine(result);
+}
+
+static string Encode(string textToEncode)
+{
+	int destinationLength = Base64.GetMaxEncodedToUtf8Length(textToEncode.Length);
+	Span<byte> result = stackalloc byte[destinationLength];
+
+	OperationStatus operationResult = Base64.EncodeToUtf8(Encoding.UTF8.GetBytes(textToEncode), result, out int bytesConsumed, out int bytesWritten);
+	if (operationResult is not OperationStatus.Done)
+		throw new Exception(operationResult.ToString());
+
+	return Encoding.UTF8.GetString(result.Slice(0, bytesWritten));
+}
